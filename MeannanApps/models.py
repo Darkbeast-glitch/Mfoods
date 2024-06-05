ec450d78ea66ea2cb1ec83edf3c9177581a9cc4f
Bethel-adjei <bethelobeng@gmail.com>
2024-06-05 14:45:46 +0000

    models

--- MeannanApps/models.py
+++ MeannanApps/models.py
@@ -17,11 +17,11 @@ class Products(models.Model):
         return f"{self.product_name}, {self.product_price}"
     
 class BillingAddress(models.Model):
-        name=models.CharField(max_length=20,blank=True,null=True)
-        emailaddress=models.EmailField( max_length=20, blank=True,null=True)
-        address=models.CharField(max_length=30,blank=True,null=True)
+        name=models.CharField(max_length=200,blank=True,null=True)
+        emailaddress=models.EmailField( blank=True,null=True)
+        address=models.CharField(max_length=200,blank=True,null=True)
         phone=models.IntegerField()
-        suggestion=models.CharField
+        
 
         class Meta:
             verbose_name= "Billing Address"
@@ -29,3 +29,50 @@ class BillingAddress(models.Model):
 
         def __str__(self) -> str:
                 return f"{self.name }"   
+
+
+class ContactUs(models.Model):
+    name=models.CharField(max_length=200, blank=True,null=True)
+    email=models.EmailField(blank=True, null=True)
+    phone=models.IntegerField()
+    subject=models.CharField(max_length=500, blank=True,null=True)
+    message=models.CharField(max_length=500, blank=True,null=True)
+
+    class Meta:
+
+        verbose_name="Contact Us"
+        verbose_name_plural="Contact Us"
+
+    
+    def __str__(self) -> str:
+        return f"{self.name, self.email,self.phone}"
+    
+
+class ShippingAddress(models.Model):
+    name=models.CharField(max_length=200, blank=True,null=True)
+    email=models.EmailField(blank=True, null=True)
+    streetaddress=models.CharField(max_length=200, blank=True,null=True)
+    phonenumber=models.IntegerField()
+    city=models.CharField(max_length=50,blank=True,null=True)
+    stateprovinceregion=models.CharField(max_length=200, blank=True,null=True)
+    zippostalcode=models.CharField(max_length=200, blank=True,null=True)
+    country=models.CharField(max_length=200, blank=True,null=True)
+
+    class Meta:
+        verbose_name="Shipping Address"
+        verbose_name_plural="Shipping Addresses"
+
+    def __str__(self):
+        return f"{self.name,self.email, self.streetaddress, self.phonenumber}"
+     
+
+class Order(models.Model):
+    products = models.ForeignKey(Products, on_delete=models.CASCADE)
+    client=models.CharField(max_length=200, blank=True,null=True)
+
+    class Meta:
+        verbose_name="Order"
+        verbose_name_plural="Orders"
+
+        def __str__(self):
+            return f"{self.products,self.client}"       
